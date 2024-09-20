762c5b320437dcdb89a45ddb34deff35a3a136a7
Andranik <andranikra@yandex.ru>
2024-09-20 23:47:20 +0400

    Готовая задача

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const deleteCard = (evt)=> evt.target.parentElement.remove();
+
+const renderCard = (deleteCard, ...initialCards) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+
+  initialCards[0].forEach(item => {
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+    cardImage.setAttribute("src", item.link);
+    cardTitle.textContent = item.name;
+
+    document.querySelector(".places__list").append(cardElement);
+    cardDeleteButton.addEventListener("click", deleteCard);
+  });
+};
+renderCard(deleteCard, initialCards);
