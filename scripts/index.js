e2b57124433184a45c52e3c5f6788ea15b5a396f
Andranik <andranikra@yandex.ru>
2024-09-23 12:02:09 +0400

    Оптимизирован код

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,28 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content; 
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-const deleteCard = (evt)=> evt.target.parentElement.remove();
-
-const renderCard = (deleteCard, ...initialCards) => {
-  const cardTemplate = document.querySelector("#card-template").content;
-
-  initialCards[0].forEach(item => {
-    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-    const cardImage = cardElement.querySelector(".card__image");
-    const cardTitle = cardElement.querySelector(".card__title");
-    const cardDeleteButton = cardElement.querySelector(".card__delete-button");
-
+const deleteCard = (evt)=> evt.target.parentElement.remove(); 
+ 
+const createCard = (deleteCard, item) => { 
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true); 
+    const cardImage = cardElement.querySelector(".card__image"); 
+    const cardTitle = cardElement.querySelector(".card__title"); 
+    const cardDeleteButton = cardElement.querySelector(".card__delete-button"); 
+ 
     cardImage.setAttribute("src", item.link);
-    cardTitle.textContent = item.name;
+    cardImage.setAttribute("alt", "Изображение природы");
+    cardTitle.textContent = item.name; 
 
-    document.querySelector(".places__list").append(cardElement);
-    cardDeleteButton.addEventListener("click", deleteCard);
-  });
-};
-renderCard(deleteCard, initialCards);
+    cardDeleteButton.addEventListener("click", deleteCard); 
+    return cardElement;
+}; 
+
+const addCard = (renderCard)=> {
+  document.querySelector(".places__list").append(renderCard); 
+}
+
+const renderInitialCards = (...initialCards) => {
+  initialCards[0].forEach(item => { 
+    addCard(createCard(deleteCard, item));
+  }); 
+}
+renderInitialCards(initialCards);
